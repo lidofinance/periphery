6f21d763f9f152c960b28ce8effc089ade09c2ed
Azat Serikov <azatsdev@gmail.com>
2023-10-11 12:42:11 +0600

    feat: enable `eventual` asserts

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,14 @@
-import { ethers } from "hardhat";
 import { afterEach, beforeEach } from "mocha";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { ethers } from "hardhat";
+
 import { EvmRevertFailed, InvalidEvmSnapshotResult } from "./errors";
 
+// enable `.eventually` asserts to avoid async/await syntax
+chai.use(chaiAsPromised);
+
+// revert the state of the blockchain back to the original state after each test
 let snapshotId: string;
 
 beforeEach(async () => {
