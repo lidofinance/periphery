7e1345c561d997241a440b0f02a70231b39c5e08
Artyom Veremeenko <arwer13@gmail.com>
2024-03-07 11:36:58 +0300

    feat(state-checker): partial support for objects as view return types

--- scripts/acceptance-tests/state-checker.ts
+++ scripts/acceptance-tests/state-checker.ts
@@ -1,12 +1,28 @@
 import { expect } from "chai";
 import { readFileSync } from "fs";
-import { parse as parseYaml } from "yaml";
-import { BaseContract, Contract, JsonRpcProvider, isAddress, getAddress } from "ethers";
+import YAML from "yaml";
+import { BaseContract, Contract, JsonRpcProvider, isAddress, getAddress, Result } from "ethers";
 
 const SUCCESS_MARK = "✔";
 const FAILURE_MARK = "❌";
 
-type ViewResult = string | boolean | bigint;
+// Contract entry fields
+enum Ef {
+  name = "name",
+  address = "address",
+  checks = "checks",
+  proxyChecks = "proxyChecks",
+  implementationChecks = "implementationChecks",
+  ozNonEnumerableAcl = "ozNonEnumerableAcl",
+}
+
+// type ArbitraryObject = { [key: string]: unknown } & { args?: never, result?: never };
+
+type ViewResultPlainValue = null | string | boolean | bigint;
+
+type ArbitraryObject = Omit<{ [key: string]: ViewResultPlainValue }, "args" | "result">;
+
+type ViewResult = ViewResultPlainValue | ArbitraryObject;
 
 type ArgsAndResult = { args: [string]; result: ViewResult; mustRevert?: boolean };
 
@@ -86,6 +102,30 @@ function loadAbi(contractName: string) {
   return JSON.parse(readFileSync(`${g_abiDirectory}/${contractName}.json`).toString());
 }
 
+function loadStateFromYaml(stateFile: string) {
+  const configContent = readFileSync(stateFile).toString();
+  const reviver = (k: unknown, v: unknown) => {
+    return typeof v === "number" ? BigInt(v) : v;
+  };
+  const state = YAML.parse(configContent, reviver);
+  return state;
+}
+
+// Supports bigint as object values
+function stringify(value: unknown) {
+  if (value instanceof Object) {
+    return JSON.stringify(
+      value,
+      (k, v) => {
+        return typeof v == "bigint" ? parseInt(v.toString()) : v;
+      },
+      2,
+    );
+  } else {
+    return `${value}`;
+  }
+}
+
 async function loadContract(contractName: string, address: string, provider: JsonRpcProvider) {
   const abi = loadAbi(contractName);
   return new Contract(address, abi, provider);
@@ -108,105 +148,14 @@ function logError(arg: unknown) {
   console.error(arg);
 }
 
-async function checkConfigEntry(
-  { address, name, checks, ozNonEnumerableAcl }: RegularContractEntry,
-  provider: JsonRpcProvider,
-) {
-  // TODO: make chai keeping full addresses in the error message
-  expect(isAddress(address), `${address} is invalid address`).to.be.true;
-  const contract: BaseContract = await loadContract(name, address, provider);
-  for (const [method, checkEntryValue] of Object.entries(checks)) {
-    if (checkEntryValue instanceof Array) {
-      for (const viewResultOrObject of checkEntryValue) {
-        await checkViewFunction(contract, method, viewResultOrObject);
-      }
-    } else {
-      await checkViewFunction(contract, method, checkEntryValue);
-    }
-  }
-
-  if (ozNonEnumerableAcl) {
-    log(`=== Non-enumerable OZ Acl checks ===`);
-    for (const role in ozNonEnumerableAcl) {
-      for (const holder of ozNonEnumerableAcl[role]) {
-        const isRoleOnHolder = await contract.getFunction("hasRole").staticCall(role, holder);
-        const logHandle = new LogCommand(`.hasRole(${role}, ${holder})`);
-        try {
-          expect(isRoleOnHolder).to.be.true;
-          logHandle.success(`${isRoleOnHolder}`);
-        } catch (error) {
-          logHandle.failure(`REVERTED with: ${(error as Error).message}`);
-        }
-      }
-    }
-  }
+function logHeader1(arg: unknown) {
+  const middleLine = `===== ${arg} =====`;
+  const headerFooter = "=".repeat(middleLine.length);
+  log(`\n${headerFooter}\n${middleLine}\n${headerFooter}`);
 }
 
-async function checkViewFunction(contract: BaseContract, method: string, expectedOrObject: ChecksEntryValue) {
-  let expected: ViewResult;
-  let args: unknown[] = [];
-  let mustRevert: boolean = false;
-  if (expectedOrObject instanceof Object && "args" in expectedOrObject) {
-    expected = expectedOrObject.result;
-    args = expectedOrObject.args;
-    mustRevert = expectedOrObject.mustRevert || false;
-  } else if (expectedOrObject === null) {
-    log(`· ${method}: skipped`);
-    return;
-  } else {
-    expected = expectedOrObject as ViewResult;
-  }
-
-  const argsStr = args.length ? `(${args.toString()})` : "";
-  const logHandle = new LogCommand(`.${method}${argsStr}`);
-
-  let actual = undefined;
-  try {
-    actual = await contract.getFunction(method).staticCall(...args);
-    if (typeof actual === "bigint" && typeof expected === "number") {
-      expected = BigInt(expected);
-    }
-    if (typeof expected === "string" && isAddress(expected)) {
-      expect(getAddress(actual)).to.equal(getAddress(expected));
-    } else {
-      expect(actual).to.equal(expected);
-    }
-    logHandle.success(actual.toString());
-  } catch (error) {
-    if (mustRevert) {
-      logHandle.success(`REVERTED with: ${(error as Error).message}`);
-    } else {
-      logHandle.failure(`REVERTED with: ${(error as Error).message}`);
-    }
-  }
-}
-
-async function checkProxyOrRegularEntry(entry: ProxyContractEntry | RegularContractEntry, provider: JsonRpcProvider) {
-  await checkConfigEntry(entry, provider);
-
-  if ("proxyChecks" in entry) {
-    log(`=== Proxy checks ===`);
-    await checkConfigEntry(
-      {
-        checks: entry.proxyChecks,
-        name: entry.proxyName,
-        address: entry.address,
-      },
-      provider,
-    );
-  }
-
-  if ("implementationChecks" in entry) {
-    log(`=== Implementation checks ===`);
-    await checkConfigEntry(
-      {
-        checks: entry.implementationChecks,
-        name: entry.name,
-        address: entry.implementation,
-      },
-      provider,
-    );
-  }
+function logHeader2(arg: unknown) {
+  log(`\n=== ${arg} ===`);
 }
 
 function getNonMutableFunctionNames(abi: Abi) {
@@ -235,12 +184,144 @@ function reportNonCoveredNonMutableChecks(
   }
 }
 
-async function checkNetworkSection(section: NetworkSection) {
+async function checkContractEntry(
+  { address, name, checks, ozNonEnumerableAcl }: RegularContractEntry,
+  provider: JsonRpcProvider,
+) {
+  expect(isAddress(address), `${address} is invalid address`).to.be.true;
+  const contract: BaseContract = await loadContract(name, address, provider);
+  for (const [method, checkEntryValue] of Object.entries(checks)) {
+    if (checkEntryValue instanceof Array) {
+      for (const viewResultOrObject of checkEntryValue) {
+        await checkViewFunction(contract, method, viewResultOrObject);
+      }
+    } else {
+      await checkViewFunction(contract, method, checkEntryValue);
+    }
+  }
+
+  if (ozNonEnumerableAcl) {
+    logHeader2("Non-enumerable OZ Acl checks");
+    for (const role in ozNonEnumerableAcl) {
+      for (const holder of ozNonEnumerableAcl[role]) {
+        const isRoleOnHolder = await contract.getFunction("hasRole").staticCall(role, holder);
+        const logHandle = new LogCommand(`.hasRole(${role}, ${holder})`);
+        try {
+          expect(isRoleOnHolder).to.be.true;
+          logHandle.success(`${isRoleOnHolder}`);
+        } catch (error) {
+          logHandle.failure(`REVERTED with: ${(error as Error).message}`);
+        }
+      }
+    }
+  }
+}
+
+function expectToEqualStruct(expected: null | ArbitraryObject, actual: Result) {
+  if (expected === null) {
+    return;
+  }
+
+  const actualAsObject = actual.toObject();
+  const errorMessage = `expected ${stringify(actualAsObject)} to equal ${stringify(expected)}`;
+
+  expect(Object.keys(actualAsObject).length, errorMessage).to.equal(Object.keys(expected).length);
+  for (const field in actualAsObject) {
+    const expectedValue = expected[field];
+    if (expectedValue === null) {
+      continue;
+    }
+    let actualValue = actualAsObject[field];
+    const errorMessageDetailed = errorMessage + ` but fields "${field}" differ`;
+    if (actualValue instanceof Result && (expectedValue as unknown) instanceof Array) {
+      actualValue = actualValue.toArray();
+    }
+    expect(actualValue, errorMessageDetailed).to.deep.equal(expectedValue);
+  }
+}
+
+async function checkViewFunction(contract: BaseContract, method: string, expectedOrObject: ChecksEntryValue) {
+  let expected: ViewResult;
+  let args: unknown[] = [];
+  let mustRevert: boolean = false;
+  if (expectedOrObject === null) {
+    log(`· ${method}: skipped`);
+    return;
+  } else if (expectedOrObject instanceof Object && "args" in expectedOrObject && "result" in expectedOrObject) {
+    expected = (expectedOrObject as ArgsAndResult).result;
+    args = (expectedOrObject as ArgsAndResult).args;
+    mustRevert = (expectedOrObject as ArgsAndResult).mustRevert || false;
+  } else {
+    expected = expectedOrObject as ViewResult;
+  }
+
+  const argsStr = args.length ? `(${args.toString()})` : "";
+  const logHandle = new LogCommand(`.${method}${argsStr}`);
+
+  let actual = undefined;
+  try {
+    actual = await contract.getFunction(method).staticCall(...args);
+    if (typeof expected === "string" && isAddress(expected)) {
+      expect(getAddress(actual)).to.equal(getAddress(expected));
+    } else if (typeof expected === "object") {
+      expectToEqualStruct(expected, actual);
+    } else {
+      expect(actual).to.equal(expected);
+    }
+    logHandle.success(stringify(actual));
+  } catch (error) {
+    if (mustRevert) {
+      logHandle.success(`REVERTED with: ${(error as Error).message}`);
+    } else {
+      logHandle.failure(`REVERTED with: ${(error as Error).message}`);
+    }
+  }
+}
+
+async function checkProxyOrRegularEntry(entry: ProxyContractEntry | RegularContractEntry, provider: JsonRpcProvider) {
+  if (Ef.checks in entry) {
+    logHeader2(Ef.checks);
+    await checkContractEntry(
+      {
+        checks: entry[Ef.checks],
+        name: entry.name,
+        address: entry.address,
+      },
+      provider,
+    );
+  }
+
+  if (Ef.proxyChecks in entry) {
+    logHeader2(Ef.proxyChecks);
+    await checkContractEntry(
+      {
+        checks: entry[Ef.proxyChecks],
+        name: entry.proxyName,
+        address: entry.address,
+      },
+      provider,
+    );
+  }
+
+  if (Ef.implementationChecks in entry) {
+    logHeader2(Ef.implementationChecks);
+    await checkContractEntry(
+      {
+        checks: entry[Ef.implementationChecks],
+        name: entry.name,
+        address: entry.implementation,
+      },
+      provider,
+    );
+  }
+}
+
+async function checkNetworkSection(section: NetworkSection, sectionTitle: string) {
   const rpcUrl = isUrl(section.rpcUrl) ? section.rpcUrl : process.env[section.rpcUrl];
   const provider = new JsonRpcProvider(rpcUrl);
   for (const contractAlias in section.contracts) {
     const entry = section.contracts[contractAlias];
-    log(`\n====== Contract: ${contractAlias} (${entry.name}, ${entry.address}) ======`);
+    logHeader1(`Contract (${sectionTitle}): ${contractAlias} (${entry.name}, ${entry.address})`);
 
     reportNonCoveredNonMutableChecks(contractAlias, "checks", entry.name, Object.keys(entry.checks));
     if (entry.proxyName) {
@@ -260,29 +341,21 @@ async function checkNetworkSection(section: NetworkSection) {
 }
 
 export async function main() {
-  const [deploymentFile, abiDir] = process.argv.slice(2);
+  const [stateFile, abiDir] = process.argv.slice(2);
   g_abiDirectory = abiDir;
 
-  const configContent = readFileSync(deploymentFile).toString();
-  const config = parseYaml(configContent);
+  const state = loadStateFromYaml(stateFile);
 
-  // log(`\n============================ CONFIG ============================`);
-  // log(configContent);
-
-  if (config.l1) {
-    log(`\n============================== L1 ==============================`);
-    await checkNetworkSection(config.l1);
+  if (state.l1) {
+    await checkNetworkSection(state.l1, "L1");
   }
 
-  if (config.l2) {
-    log(`\n============================== L2 ==============================`);
-    await checkNetworkSection(config.l2);
+  if (state.l2) {
+    await checkNetworkSection(state.l2, "L2");
   }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
+  logError(error);
   process.exitCode = 1;
 });
